a39c5a36bd2e3a72c5f356c765da6cea69f561d3
Mohammad Mahdi <mohdi.gh87@gmail.com>
2025-10-01 00:19:09 +0330

    Add files via upload

---
+++ src/gsap.js
@@ -0,0 +1,4 @@
+import { gsap } from "gsap";
+import { MorphSVGPlugin } from "gsap/MorphSVGPlugin";
+import { Physics2DPlugin } from "gsap/Physics2DPlugin";
+gsap.registerPlugin(MorphSVGPlugin, Physics2DPlugin);
